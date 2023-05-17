6db908dda8b0317469dc2293460e24acfbb96035
Kayode Odeyemi <larsody@gmail.com>
2023-05-17 09:32:32 +0000

    correction

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python3
 """
-Given a pile of coins of different values,
-    determine the fewest number of coins needed to meet
-    a given amount total.
+Making Change
 """
-
-import sys
 
 
 def makeChange(coins, total):
+
     """
-    Return: fewest number of coins needed to meet total
-    If total is 0 or less, return 0
-    If total cannot be met by any number of coins you have, return -1
+    Logic for the fewest number of coins needed
+    to meet a given amount total.
     """
+
     if total <= 0:
         return 0
-    table = [sys.maxsize for i in range(total + 1)]
-    table[0] = 0
-    m = len(coins)
-    for i in range(1, total + 1):
-        for j in range(m):
-            if coins[j] <= i:
-                subres = table[i - coins[j]]
-                if subres != sys.maxsize and subres + 1 < table[i]:
-                    table[i] = subres + 1
-
-    if table[total] == sys.maxsize:
-        return -1
-    return table[total]
+    remaining_total = total
+    coins_count = 0
+    coin_index = 0
+    sorted_coins = sorted(coins, reverse=True)
+    num = len(coins)
+    while remaining_total > 0:
+        if coin_index >= num:
+            return -1
+        if remaining_total - sorted_coins[coin_index] >= 0:
+            remaining_total -= sorted_coins[coin_index]
+            coins_count += 1
+        else:
+            coin_index += 1
+    return coins_count
