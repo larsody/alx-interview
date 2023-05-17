c41e4d5d88b18d4a514821cb82abffc713ba2e63
Kayode Odeyemi <larsody@gmail.com>
2023-05-17 09:22:14 +0000

    update

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-'''Given a pile of coins of different values,
+"""
+Given a pile of coins of different values,
     determine the fewest number of coins needed to meet
     a given amount total.
-'''
+"""
+
 import sys
 
 
 def makeChange(coins, total):
-    '''
+    """
     Return: fewest number of coins needed to meet total
     If total is 0 or less, return 0
     If total cannot be met by any number of coins you have, return -1
-    '''
+    """
     if total <= 0:
         return 0
     table = [sys.maxsize for i in range(total + 1)]
